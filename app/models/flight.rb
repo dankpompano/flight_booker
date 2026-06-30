cfe3f0633f99eebbc79139ab100cb714166700d0
Jacob <jacoblittler2003@gmail.com>
2026-06-29 22:14:55 -0400

    .

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_ids"
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_ids"
+
+  def flight_date_formatted
+    flight_date.strftime("%m/%d/%Y")
+  end
 end
