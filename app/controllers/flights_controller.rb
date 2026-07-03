966ac19c1275a54a4b21da704b671a9db2816f3f
Jacob <jacoblittler2003@gmail.com>
2026-07-03 11:37:19 -0400

    Screen 1:search completed

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,23 @@
 class FlightsController < ApplicationController
+  def new
+    @flight = Flight.new
+  end
+
+  def edit
+    @flight = Flight.find(params[:id])
+  end
+  
   def index
-    @flights = Flight.all
+    if params[:search].present?
+      @flights = Flight.where("title LIKE ?", "%#{params[:search]}%")
+      @airport = Airport.find(params[:id])
+    else 
+      @flights = Flight.all
+    end
+  end
+
+  private
+  def flight_params
+    params.require(:flight).permit(:arrival_id, :departure_ids, :flight_duration, :start)
   end
 end
