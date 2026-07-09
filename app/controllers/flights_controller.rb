dd7c4f6aeab0c75feabd17a750ad23436217ae0b
Jacob <jacoblittler2003@gmail.com>
2026-07-09 06:40:15 -0400

    radio buttons

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -15,6 +15,13 @@ class FlightsController < ApplicationController
     @flights = @flights.where(start: params[:start]) if params[:start].present?
   end
 
+  def results
+    @flights = Flight.all
+    @flights = @flights.where(arrival_ids: params[:arrival_ids]) if params[:arrival_ids].present?
+    @flights = @flights.where(departure_ids: params[:departure_ids]) if params[:departure_ids].present?
+    @flights = @flights.where(pass_num: params[:pass_num]) if params[:pass_num].present?
+    @flights = @flights.where(start: params[:start]) if params[:start].present?  end
+
   private
   def flight_params
     params.require(:flight).permit(:arrival_id, :departure_ids, :flight_duration, :start)
