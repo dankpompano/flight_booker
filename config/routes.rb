61c3ac3bcaeb94defba2508e60e98109cbc84138
Jacob <jacoblittler2003@gmail.com>
2026-07-09 18:33:41 -0400

    Phase 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get "flights/index"
   resources :flights, :bookings
   get 'search/results', to: 'flights#results', as: 'search_results'
+  get 'results/flights', to:'bookings#new', as: 'booking_submission'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
